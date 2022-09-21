cfe595ee725fb9322366ece67fef227835b341e8
Monica Oktaviona <monicaoktaviona10@gmail.com>
2022-09-21 22:38:28 +0700

    Menambahkan unit test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
-# Create your tests here.
+class mywatchlist_test(TestCase):
+    def test_url_is_exist_html(self):
+        response = Client().get("/mywatchlist/html/")
+        self.assertEqual(response.status_code, 200)
+    def test_url_is_exist_xml(self):
+        response = Client().get("/mywatchlist/xml/")
+        self.assertEqual(response.status_code, 200)
+    def test_url_is_exist_json(self):
+        response = Client().get("/mywatchlist/json/")
+        self.assertEqual(response.status_code, 200)
+
+
+
+
